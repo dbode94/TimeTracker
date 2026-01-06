f5cdedf839d2b9efc5f52cca202a06b99957c804
Dennis Bode <dbode0694@gmail.com>
2026-01-06 00:33:10 -0500

    added new animations to input lables and a show password checkbox has been added.

--- public/login/login.js
+++ public/login/login.js
@@ -3,7 +3,8 @@
 //============================================================================================
 let inputs = document.getElementsByTagName('input');
 let labels = document.getElementsByTagName('label');
-
+let emojiDiv = document.getElementById('logoContiner');
+let showPassword = document.getElementById('showPasswordCB');
 
 //============================================================================================
 // Defining functions
@@ -30,22 +31,54 @@ const movelabelDown = (e) => {
     }
 }
 
+//TODO: Make it a class with a state to handle actions better
+const changeEmojiState = (emotion) => {
+    switch (emotion) {
+        case 'surprise':
+            emojiDiv.innerText = '😱'
+            break;
+
+        case 'focus':
+            emojiDiv.innerText = '🤔'
+            break;
+
+        case 'hide':
+            emojiDiv.innerText = '🫣'
+            break;
+    
+        default:
+            emojiDiv.innerText = '🫡'
+            break;
+    }
+}
 
 //============================================================================================
 // Defining Event Handlers
 //============================================================================================
 const checkInputState = (e) => {
-    if(e.type == 'focus' && e.target.value == '')
+    if(e.type == 'focus' && e.target.value == ''){
         movelabelUp(e);
-    else if(e.type == 'blur' && e.target.value == '')
+    }
+    else if(e.type == 'blur' && e.target.value == ''){
         movelabelDown(e);
+    }
 }
 
+const showPasswordHandler = (e) => {
+    if(e.target.checked){
+        document.getElementById('password').type= 'text'
+    }
+    else {
+        document.getElementById('password').type= 'password'
+    }
 
+}
 //============================================================================================
 // Adding Event Listeners
 //============================================================================================
 for (let i = 0; i < inputs.length; i++){
     inputs[i].addEventListener('focus', checkInputState);
-    inputs[i].addEventListener('blur', checkInputState);  //for when the input loses focus
+    inputs[i].addEventListener('blur', checkInputState);
 }
+
+showPassword.addEventListener('change', showPasswordHandler)
