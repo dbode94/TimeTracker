d0c7feb37a19b4a51c82ffbdbf058d4ab1ff0978
Dennis Bode <dbode0694@gmail.com>
2026-02-02 15:57:50 -0500

    Get timers API and front implemented to populate timer list

--- controllers/getAllTimers.js
+++ controllers/getAllTimers.js
@@ -2,13 +2,14 @@ import {collection, getDocs} from 'firebase/firestore'
 
 export function getAllTimers (db, req, res) {
     if(!!req.session?.userId) {
-
-        console.log(req.session.userId)
-        
         const userId = req.session.userId
         const collectionRef = collection(db, 'users', userId, 'timers');
         getDocs(collectionRef)
-        .then((docs) => docs.forEach(doc => console.log(doc.data)))
-        .catch((error) => console.error('ERROR Fetcing:', error))
+        .then((docs) => {
+            const timers = [];
+            docs.forEach(doc => timers.push({...doc.data(), id: doc.id}))                    
+            res.status(200).json(timers)
+        })
+        .catch((error) => res.status(400).json('Unable to fetch timers'))
     }
 }
